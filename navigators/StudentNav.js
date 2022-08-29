000a714ec78972281ff393c34a80411503a2657a
Haziq Musa <last.game01987@gmail.com>
2022-08-29 20:37:39 +0800

    add Cafe List screen utk backup klau qr code prob

--- navigators/StudentNav.js
+++ navigators/StudentNav.js
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import PayNow from "../pages/PayNow";
 import QRScan from "../pages/QRScan";
+import CafeList from "../pages/CafeList";
 import StudentDashboard from "../pages/StudentDashboard";
 
 const Stack = createNativeStackNavigator();
@@ -25,6 +26,14 @@ const StudentNav = () => {
         name="QR Scan"
         component={QRScan}
         options={{
+          headerTitleAlign: "center",
+        }}
+      />
+      <Stack.Screen
+        name="Cafe List"
+        component={CafeList}
+        options={{
+          title: "Confirmation",
           headerTitleAlign: "center",
         }}
       />
