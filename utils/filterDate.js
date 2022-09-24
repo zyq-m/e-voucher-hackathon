d4bd570026c9594d63abb7db1abaa1891c353626
Haziq Musa <last.game01987@gmail.com>
2022-09-25 03:59:38 +0800

    refactor code

--- utils/filterDate.js
+++ utils/filterDate.js
@@ -6,14 +6,14 @@ export const useFilterDate = () => {
 
   const filterDate = arr => {
     // filter by current day
-    const today = arr.filter(data => {
+    const getToday = () => arr.filter(data => {
       const date = moment(data.created_at).format(dateFormat);
 
       return date === currentDate.format(dateFormat);
     });
 
     // filter by current week
-    const currentWeek = arr.filter(data => {
+    const getWeek = () => arr.filter(data => {
       const firstDay = currentDate.startOf("week").dayOfYear()
       const lastDay = currentDate.endOf("week").dayOfYear()
       const date = moment(data.created_at).dayOfYear()
@@ -22,16 +22,16 @@ export const useFilterDate = () => {
     });
 
     // filter by current month
-    const currentMonth = arr.filter(data => {
+    const getMonth = () => arr.filter(data => {
       const date = moment(data.created_at).month();
 
-      return date === currentDate.month() - 1;
+      return date === currentDate.month();
     });
 
+    const getAll = () => arr
+
     return {
-      today: today,
-      week: currentWeek,
-      month: currentMonth,
+      getAll, getToday, getWeek, getMonth
     };
   };
 
