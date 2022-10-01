1eca0a8d361d5608e410ace38a58336955175c0e
Haziq Musa <last.game01987@gmail.com>
2022-10-01 21:27:35 +0800

    kumpulkan file pages dlm 1 file

--- App.js
+++ App.js
@@ -3,16 +3,16 @@ import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Login from "./pages/Login";
-import Transaction from "./pages/Transaction";
-import TransactionDetail from "./pages/TransactionDetail";
-import Dashboard from "./pages/Dashboard";
-
-import PayNow from "./pages/PayNow";
-import QRScan from "./pages/QRScan";
-import CafeList from "./pages/CafeList";
-import MyQRCode from "./pages/MyQRCode";
-
+import {
+  CafeList,
+  Dashboard,
+  Login,
+  MyQRCode,
+  PayNow,
+  QRScan,
+  Transaction,
+  TransactionDetail,
+} from "./pages";
 import { UserContext } from "./lib/Context";
 
 const Stack = createNativeStackNavigator();
@@ -30,13 +30,13 @@ export default function App() {
       <UserContext.Provider value={{ user, setUser }}>
         <Stack.Navigator
           initialRouteName="login"
-          screenOptions={{ headerTitleAlign: "center", animation: "fade_from_bottom" }}>
+          screenOptions={{
+            headerTitleAlign: "center",
+            animation: "fade_from_bottom",
+          }}>
           {user.login && (
             <>
-              <Stack.Screen
-                name="QR Scan"
-                component={QRScan}
-              />
+              <Stack.Screen name="QR Scan" component={QRScan} />
               <Stack.Screen
                 name="Pay"
                 component={PayNow}
@@ -51,19 +51,13 @@ export default function App() {
                   title: "Confirmation",
                 }}
               />
-              <Stack.Screen
-                name="My QRCode"
-                component={MyQRCode}
-              />
+              <Stack.Screen name="My QRCode" component={MyQRCode} />
               <Stack.Screen
                 name="Dashboard"
                 component={Dashboard}
                 options={{ headerShown: false }}
               />
-              <Stack.Screen
-                name="Transactions"
-                component={Transaction}
-              />
+              <Stack.Screen name="Transactions" component={Transaction} />
               <Stack.Screen
                 name="Transaction Details"
                 component={TransactionDetail}
