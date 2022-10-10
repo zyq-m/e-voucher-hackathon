d4f9e711cf89b4fa3b39fccfe38b1fbdf818608d
Haziq Musa <last.game01987@gmail.com>
2022-10-11 00:09:51 +0800

    to string bfr store

--- utils/SecureStore.js
+++ utils/SecureStore.js
@@ -2,25 +2,25 @@ import * as SecureStore from "expo-secure-store";
 import { Platform } from "react-native";
 
 export const save = async (key, value) => {
-  if (Platform.OS === 'web') {
-    return localStorage.setItem(key, value)
+  if (Platform.OS === "web") {
+    return localStorage.setItem(key, JSON.stringify(value));
   } else {
-    await SecureStore.setItemAsync(key, value);
+    await SecureStore.setItemAsync(key, JSON.stringify(value));
   }
-}
+};
 
 export const getValueFor = async key => {
-  if (Platform.OS === 'web') {
-    return localStorage.getItem(key)
+  if (Platform.OS === "web") {
+    return localStorage.getItem(key);
   } else {
     return await SecureStore.getItemAsync(key);
   }
-}
+};
 
 export const deleteItem = async key => {
-  if (Platform.OS === 'web') {
-    return localStorage.removeItem(key)
+  if (Platform.OS === "web") {
+    return localStorage.removeItem(key);
   } else {
     return await SecureStore.deleteItemAsync(key);
   }
-}
+};
