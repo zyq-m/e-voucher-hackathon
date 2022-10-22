a258f813f31e972e64fd3d9cdd3614d5c866e2cf
Haziq Musa <last.game01987@gmail.com>
2022-10-22 16:21:08 +0800

    update app.json file

--- android/app/src/main/jni/Android.mk
+++ android/app/src/main/jni/Android.mk
@@ -10,7 +10,7 @@ include $(CLEAR_VARS)
 LOCAL_PATH := $(THIS_DIR)
 
 # You can customize the name of your application .so file here.
-LOCAL_MODULE := b40ewallet_appmodules
+LOCAL_MODULE := evoucer_appmodules
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/*.cpp)
